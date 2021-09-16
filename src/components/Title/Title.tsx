1e646c07d86f4d2681b79483bcb4cab37c6b2331
Benjamin Dekens <benjamin.dekens@live.fr>
2021-09-16 19:38:54 +0200

    feat(title): add style to title

--- src/components/Title/Title.tsx
+++ src/components/Title/Title.tsx
@@ -11,10 +11,10 @@ type TitleProps = {
 const Title: React.FC<TitleProps> = (props) => {
   const { operation, currency } = props;
   return (
-    <>
+    <div className={styles.titleWrapper}>
       <div className={styles.operation}>{ operation }</div>
       <div className={styles.currency}>{currency}</div>
-    </>
+    </div>
   );
 };
 
