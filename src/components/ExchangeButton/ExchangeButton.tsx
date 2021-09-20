89a2e322abff7c1828e2926a28a9d3da279893b2
Benjamin Dekens <benjamin.dekens@live.fr>
2021-09-20 23:30:06 +0200

    feat(button): add exchange button text

--- src/components/ExchangeButton/ExchangeButton.tsx
+++ src/components/ExchangeButton/ExchangeButton.tsx
@@ -1,12 +1,28 @@
 import Button from '@mui/material/Button';
 import React from 'react';
+import { useStoreState } from '../../store/hooks';
 import styles from './ExchangeButton.module.css';
 
 type ExchangeButtonProps = {
 }
 
-const ExchangeButton: React.FC<ExchangeButtonProps> = () => (
-  <Button variant="contained">Contained</Button>
-);
+const ExchangeButton: React.FC<ExchangeButtonProps> = () => {
+  const operation = useStoreState((state) => state.operation);
+  const currencyIn = useStoreState((state) => state.currencyIn);
+  const currencyOut = useStoreState((state) => state.currencyOut);
+  return (
+    <div className={styles.exchangeButtonWrapper}>
+      <Button className={styles.button} variant="contained">
+        {operation}
+        {' '}
+        {currencyIn}
+        {' '}
+        {operation === 'Sell' ? 'for' : 'from'}
+        {' '}
+        {currencyOut}
+      </Button>
+    </div>
+  );
+};
 
 export default ExchangeButton;
