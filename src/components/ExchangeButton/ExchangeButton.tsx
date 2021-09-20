5bbd4012f068ad51bffe4c51022115f6b1fa5a6d
Benjamin Dekens <benjamin.dekens@live.fr>
2021-09-20 23:36:29 +0200

    feat(button): disable button when the balance is insufficient

--- src/components/ExchangeButton/ExchangeButton.tsx
+++ src/components/ExchangeButton/ExchangeButton.tsx
@@ -9,10 +9,40 @@ type ExchangeButtonProps = {
 const ExchangeButton: React.FC<ExchangeButtonProps> = () => {
   const operation = useStoreState((state) => state.operation);
   const currencyIn = useStoreState((state) => state.currencyIn);
+  const currencyInValue = useStoreState((state) => state.currencyInValue);
   const currencyOut = useStoreState((state) => state.currencyOut);
+  const currencyOutValue = useStoreState((state) => state.currencyOutValue);
+
+  const balanceIn = useStoreState(
+    (state) => state.accountsList
+      .filter((account) => account.currency === currencyIn)[0].balance,
+  );
+  const balanceOut = useStoreState(
+    (state) => state.accountsList
+      .filter((account) => account.currency === currencyOut)[0].balance,
+  );
+
+  let disableButton = false;
+  if (
+    operation === 'Sell'
+    && currencyInValue
+    && currencyInValue > balanceIn
+  ) {
+    disableButton = true;
+  } else if (
+    operation === 'Buy'
+    && currencyOutValue
+    && currencyOutValue > balanceOut
+  ) {
+    disableButton = true;
+  }
   return (
     <div className={styles.exchangeButtonWrapper}>
-      <Button className={styles.button} variant="contained">
+      <Button
+        className={styles.button}
+        variant="contained"
+        disabled={disableButton}
+      >
         {operation}
         {' '}
         {currencyIn}
